143c759f5d0de64e1cdf0e76b694a15bb3be16a1
Andrew Mokin <parkovka.mokin@yandex.ru>
2024-02-04 09:02:09 +0300

    lesson_1_art

---
+++ lesson1/asciiart.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void){
+    printf(".--.\n");
+    printf("'--'\n");
+    printf("\n");
+    printf(" /\\\n");
+    printf("/__\\\n");
+    printf("\n");
+    printf("           ____  \n");
+    printf("(^._.^)~ <(o.o )>\n");
+    printf("\n");
+    printf("  |\\_/|\n");
+    printf(" / @ @ \\\n");
+    printf("( > o < )\n");
+    printf(" '>>x<<'\n");
+    printf(" /  O  \\\n");
+    return 0;
+}
